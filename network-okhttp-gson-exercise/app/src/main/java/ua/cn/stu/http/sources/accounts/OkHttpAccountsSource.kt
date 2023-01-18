6636ede18c2db93a76360ba2d87a98a3767a973e
ArsenKoch <arsenkocaran@icloud.com>
2023-01-18 22:02:39 +0300

    Implement method: getAccount() -> for fetching account info

--- network-okhttp-gson-exercise/app/src/main/java/ua/cn/stu/http/sources/accounts/OkHttpAccountsSource.kt
+++ network-okhttp-gson-exercise/app/src/main/java/ua/cn/stu/http/sources/accounts/OkHttpAccountsSource.kt
@@ -5,6 +5,7 @@ import okhttp3.Request
 import ua.cn.stu.http.app.model.accounts.AccountsSource
 import ua.cn.stu.http.app.model.accounts.entities.Account
 import ua.cn.stu.http.app.model.accounts.entities.SignUpData
+import ua.cn.stu.http.sources.accounts.entities.GetAccountResponseEntity
 import ua.cn.stu.http.sources.accounts.entities.SignInRequestEntity
 import ua.cn.stu.http.sources.accounts.entities.SignInResponseEntity
 import ua.cn.stu.http.sources.accounts.entities.SignUpRequestEntity
@@ -12,7 +13,6 @@ import ua.cn.stu.http.sources.base.BaseOkHttpSource
 import ua.cn.stu.http.sources.base.OkHttpConfig
 
 // todo #6: implement methods:
-//          - getAccount() -> for fetching account info
 //          - setUsername() -> for editing username
 class OkHttpAccountsSource(
     config: OkHttpConfig
@@ -24,7 +24,7 @@ class OkHttpAccountsSource(
             email = email,
             password = password
         )
-        val request = okhttp3.Request.Builder()
+        val request = Request.Builder()
             .post(signInRequestEntity.toJsonRequestBody())
             .endpoint("/sign-in")
             .build()
@@ -48,9 +48,13 @@ class OkHttpAccountsSource(
 
     override suspend fun getAccount(): Account {
         delay(1000)
-        // Call "GET /me" endpoint.
-        // Use GetAccountResponseEntity.
-        TODO()
+        val request = Request.Builder()
+            .get()
+            .endpoint("/me")
+            .build()
+        val response = client.newCall(request).suspendEnqueue()
+        val accountEntity = response.parseJsonResponse<GetAccountResponseEntity>()
+        return accountEntity.toAccount()
     }
 
     override suspend fun setUsername(username: String) {
