379af31efebb67204ea65a99ee110f6477eed9d3
ArsenKoch <arsenkocaran@icloud.com>
2023-01-20 19:05:50 +0300

    Create SourcesProvider

--- network-okhttp-gson-exercise/app/src/main/java/ua/cn/stu/http/sources/SourceProviderHolder.kt
+++ network-okhttp-gson-exercise/app/src/main/java/ua/cn/stu/http/sources/SourceProviderHolder.kt
@@ -1,30 +1,42 @@
 package ua.cn.stu.http.sources
 
+import com.google.gson.Gson
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import ua.cn.stu.http.app.Const
 import ua.cn.stu.http.app.model.SourcesProvider
 import ua.cn.stu.http.app.model.settings.AppSettings
+import ua.cn.stu.http.sources.base.OkHttpConfig
+import ua.cn.stu.http.sources.base.OkHttpSourcesProvider
 
 object SourceProviderHolder {
 
     val sourcesProvider: SourcesProvider by lazy<SourcesProvider> {
-        TODO("#12: Create SourcesProvider. " +
-                "Hint: use Const.BASE_URL as a value for 'baseUrl' argument")
+        val config = OkHttpConfig(
+            baseUrl = Const.BASE_URL,
+            client = createOkHttpClient(),
+            gson = Gson()
+        )
+        OkHttpSourcesProvider(config)
     }
 
     private fun createOkHttpClient(): OkHttpClient {
-        TODO("#11: create OkHttpClient by using OkHttpClient.Builder. " +
-                "Add 2 interceptors: for authorization and for logging requests.")
+        TODO(
+            "#11: create OkHttpClient by using OkHttpClient.Builder. " +
+                    "Add 2 interceptors: for authorization and for logging requests."
+        )
         // Hint: use 'Singletons.appSettings' as an implementation of AppSettings interface.
     }
 
     private fun createAuthorizationInterceptor(settings: AppSettings): Interceptor {
-        TODO("#10: create an Interceptor which adds Authorization header if " +
-                "there is a token in the app settings")
+        TODO(
+            "#10: create an Interceptor which adds Authorization header if " +
+                    "there is a token in the app settings"
+        )
     }
 
     private fun createLoggingInterceptor(): Interceptor {
+
         TODO("#9: create HttpLoggingInterceptor")
     }
-
 }
